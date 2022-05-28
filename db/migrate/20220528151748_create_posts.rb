7a52ebcd7d2876ec836d6a3e45fe3ad81920d5a3
Yaro <yshmarov@gmail.com>
2022-05-28 17:23:29 +0200

    scaffold posts

---
+++ db/migrate/20220528151748_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :access
+      t.string :passcode
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
